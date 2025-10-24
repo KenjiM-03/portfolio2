dc7f6ed3c59baa0530860ed3ed9cb699a3c0c4fc
KenjiM-03 <kenjimende7@gmail.com>
2025-10-24 11:12:56 +0800

    init commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1 class = "text-red-400">Welcome to your library project</h1>
+<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+<p class = "text-green-500">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
