d073b893bb6733beaed15b52c90daba1f8a7f347
KenjiM-03 <kenjimende7@gmail.com>
2025-10-27 16:25:33 +0800

    basta

---
+++ src/components/Header.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let y;
+    let tabs = [
+        {name: 'Projects', link:'#projects'},
+        {name: 'About me',link:'#projects'},
+        {name: 'Gallery',link:'#projects'},
+
+    ];
+</script>
+<header class={'sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border border-solid' + (
+    y=0 ? 'py-4 bg-slate-950 border-violet-900 ': ' py-6 bg-transparent border-transparent'
+)}>
+    <h1 class="font-medium">
+        <b class="font-bold poppins">Hugh </b> Jazz
+
+    </h1>
+    <div class="sm:flex items-center gap-4 hidden">
+        {#each tabs as tab, index}
+            <a>
+                <p>{tab.name}</p>
+            </a>
+        {/each}
+    </div>
+</header>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,34 @@
 <script lang="ts">
-	import '../app.css';
+    import '../app.css';
+    import Footer from "../components/Footer.svelte";
+    import Header from "../components/Header.svelte";
 
-	let { children } = $props();
+    let y = $state(0);
+    let innerHeight = $state(0);
+    let innerWidth = $state(0);
+
+    function goTop() {
+        document.body.scrollIntoView({ behavior: "smooth" });
+    }
+
+    let { children } = $props();
 </script>
 
-{@render children()}
+<div class="relative flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen pointer-events-none">
+
+    <div class="fixed bottom-0 right-0 duration-200 flex p-10 z-[10] pointer-events-auto">
+        {#if y > 0}
+            <button onclick={goTop} class="bg-purple-600 text-white p-3 rounded-full shadow-lg" aria-label="Scroll to top">
+                <i class="fa-solid fa-arrow-up"></i>
+            </button>
+        {/if}
+    </div>
+
+    <div class="pointer-events-auto">
+        <Header y={y}/>
+        {@render children()}
+        <Footer />
+    </div>
+</div>
+
+<svelte:window bind:scrollY={y} bind:innerHeight bind:innerWidth />
