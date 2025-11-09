6538362103a2c5db58866c97e4b9d77f93839e63
KenjiM-03 <kenjimende7@gmail.com>
2025-11-09 21:33:04 +0800

    added home

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,1 +1,40 @@
+
+<script>
+
+</script>
+<main>
+    <section id = "intropage" class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14">
+        <div class ="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
+            <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+                My name <span class="poppins text-[#47945A]">Jeff</span> Bezos<br/> Absolute <span class="poppins text-[#FFFDA7]">
+            Developer
+            </span>
+            </h2>
+            <p class="text-base sm:text-lg md:text-xl">
+                My name <span class="poppins text-[#47945A]">Jeff</span> Bezos<br/> Absolute <span class="poppins text-[#FFFDA7]">
+            Developer
+            </span>
+            </p>
+            <a class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-5 py-2 group rounded-full bg-white text-slate-950 cursor-pointer" 
+               href="#"
+               target="_blank"
+               >
+                <div class="absolute top-0 right-full w-full h-full1 bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200"></div>
+                <h4 class="relative z-[10]">Contact me &rarr;</h4>
+            </a>
+        </div>
+        <div class="relative shadow-2xl grid place-items-center w-fit mx-auto">
+            <img
+                    src={"images/profile.jpg"}
+                    alt="Zetane Engine"
+                    class="object-cover z-[2] max-h-[70vh] shadow-lg"
+            />
+        </div>
+
+    </section>
+
+    <section id="projects" class="py">
+
+    </section>
+</main>
 <h1>skibidi</h1>
